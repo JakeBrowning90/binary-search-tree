5f308b1ae484fa34d70af87652277824199b3cc2
jakebrowning90 <jacobbrowning90@gmail.com>
2023-01-29 10:36:50 -0500

    Add placeholders for functions

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log("Hello!")
+
+// Node factory
+class Node {
+    constructor(value = null, leftNode = null, rightNode = null) {
+        this.value = value;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+}
+
+// Tree factory
+class Tree {
+    constructor() {
+        this.root = buildTree();
+    }
+
+    buildTree(array) {
+        // Sort the array
+        // Remove duplicates
+        // Set node
+        // Make left branch
+        // Make right branch
+    }
+
+    //insert() {}
+    //delete() {}
+    //find() {}
+    //levelOrder() {}
+    //inorder() {}
+    //preorder() {}
+    //postorder() {}
+    //height() {}
+    //depth() {}
+    //isBalanced() {}
+    //rebalance() {}
+}
+
+const testTree = new Tree;
