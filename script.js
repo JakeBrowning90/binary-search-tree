b980b2c3672b2f80da4a08691599212864e81be4
jakebrowning90 <jacobbrowning90@gmail.com>
2023-01-30 18:17:38 -0500

    Add basic tree building function

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-console.log("Hello!")
-
 // Node factory
 class Node {
-    constructor(value = null, leftNode = null, rightNode = null) {
+    constructor(value = null) {
         this.value = value;
-        this.leftNode = leftNode;
-        this.rightNode = rightNode;
+        this.leftNode = null;
+        this.rightNode = null;
     }
 }
 
 // Tree factory
 class Tree {
-    constructor() {
-        this.root = buildTree();
+    constructor(array) {
+        this.root = this.buildTree(array);
+    }
+
+    sortArray(a, b) {
+        return a - b;
+    }
+
+    removeDuplicates(array) {
+        return array.filter((item, index) => array.indexOf(item) === index);
     }
 
     buildTree(array) {
         // Sort the array
+        let sortedArray = array.sort(this.sortArray);
         // Remove duplicates
-        // Set node
-        // Make left branch
-        // Make right branch
+        sortedArray = this.removeDuplicates(sortedArray);
+        // Get length of array, needed to determine midpoint
+        let l = sortedArray.length;
+        // Set root and begin recursion
+        let root = this.sortedArrayToBST(sortedArray, 0, l - 1);
+        return root;
+    }
+
+    sortedArrayToBST(array, start, end) {
+        // Base case: array "half" is now empty and can make no more Nodes
+        if (start > end)
+        {
+            return null;
+        }
+        // Get the middle element of the array and set it as the root Node
+        let mid = parseInt((start + end) / 2);
+        var node = new Node(array[mid]);
+        // Recursively construct the left branch
+        node.leftNode = this.sortedArrayToBST(array, start, mid - 1);
+         // Recursively construct the right branch
+        node.rightNode = this.sortedArrayToBST(array, mid + 1, end);
+        return node;
     }
 
     //insert() {}
@@ -36,4 +62,18 @@ class Tree {
     //rebalance() {}
 }
 
-const testTree = new Tree;
+let sampleArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const testTree = new Tree(sampleArray);
+console.log(testTree);
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.rightNode !== null) {
+      prettyPrint(node.rightNode, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+    if (node.leftNode !== null) {
+      prettyPrint(node.leftNode, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+
+prettyPrint(testTree.root);
